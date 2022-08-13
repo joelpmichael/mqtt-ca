2a19bb3891905ca2a185774b9c17a7c27e51e9d1
Joel Michael <joelpmichael@gmail.com>
2022-08-13 21:03:41 +1000

    first mqtt bits

--- mqtt-ca.py
+++ mqtt-ca.py
@@ -1,6 +1,33 @@
 #!/usr/bin/env python3
 
 import time
+import paho.mqtt.client as mqtt
+import ssl
 
-while True:
-    time.sleep(1)
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("$SYS/#")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+    
+if __name__ == "__main__":
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+#    client.tls_set(ca_certs=None, certfile=None, keyfile=None, cert_reqs=ssl.CERT_REQUIRED,
+#        tls_version=ssl.PROTOCOL_TLS, ciphers=None)
+
+    client.connect("localhost", 8883, 60)
+
+    # Blocking call that processes network traffic, dispatches callbacks and
+    # handles reconnecting.
+    # Other loop*() functions are available that give a threaded interface and a
+    # manual interface.
+    client.loop_forever()
